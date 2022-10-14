a158bab2a719d5e49aee04dc8494354aed4a6b8a
hndwkwksn <hondawakuwakusan0630@gmail.com>
2022-10-14 22:59:36 +0900

    [Add]ClassUser

---
+++ src/main/java/com/example/demo/data/entity/User.java
@@ -0,0 +1,38 @@
+package com.example.demo.data.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String email;
+	
+	public User() {
+	}
+	
+	public Long getId() {
+		return this.id;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
